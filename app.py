c1d99eb1d0c713c0216adbb3085e2353adb76db3
namtoro10-dot <namtoro19@gmail.com>
2025-12-22 01:12:33 +0700

    Fix app.py bind port for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string
+import os
 
 app = Flask(__name__)
 
@@ -93,6 +94,7 @@ def home():
     return render_template_string(HTML)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-print("App da chạy")
+
